b27f02060d971afa02fbf35fa30f238b268212f3
Carlos Abraão Chaves <carlos.programador@gmail.com>
2023-12-04 19:17:46 -0300

    mapeando e enumerando as cartas do jogo

--- src/scripts/engine.js
+++ src/scripts/engine.js
@@ -1,7 +1,7 @@
 
 const state = {
 
-
+   /**LIGANDO OS ELEMENTOS COM O HTML INICIO */
     score: {
         playerScore: 0,
         computerScore: 0,
@@ -22,11 +22,60 @@ const state = {
         player: document.getElementById("computer-field-card"),
     },
 
-    button: document.getElementById("next-duel")
+    action:{
+        button: document.getElementById("next-duel"),
+    },
+    
 };
 
+/**LIGANDO OS ELEMENTOS COM O HTML FIM */
 
 
+/**MAPEANDO AS CARTAS INICIO */
+
+const pathImages = ".src/assets/icons/";
+const cardData =[
+{
+    id:0, name: "Blue Eyes white Dragon", type: "Paper", img: `${pathImages}dragon.png`,
+
+    winOf:[1],
+    loseOf:[2],
+
+
+
+},
+
+{
+    id:1, name: "Dark Magician", type: "Rock", img: `${pathImages}magician.png`,
+
+    winOf:[2],
+    loseOf:[0],
+
+
+
+},
+
+
+{
+    id:2, name: "Exodia", type: "Scissors", img: `${pathImages}exodia.png`,
+
+    winOf:[0],
+    loseOf:[1],
+
+
+
+},
+
+
+
+
+
+];
+
+/**MAPEANDO AS CARTAS FIM */
+
+
+/**METODO DE INICIALIZAÇÃO */
 function init(){
 
     
