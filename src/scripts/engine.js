9172f440329be40c193403e964d5c4847c66f15b
Carlos Abraão Chaves <carlos.programador@gmail.com>
2023-12-04 19:00:25 -0300

    relacionando o javascript com os elementos do html

---
+++ src/scripts/engine.js
@@ -0,0 +1,37 @@
+
+const state = {
+
+
+    score: {
+        playerScore: 0,
+        computerScore: 0,
+        scoreBox: document.getElementById('score_points'),
+
+    },
+
+    cardSprites:{
+
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+    },
+
+
+    fieldCards:{
+        player: document.getElementById("player-field-card"),
+        player: document.getElementById("computer-field-card"),
+    },
+
+    button: document.getElementById("next-duel")
+};
+
+
+
+function init(){
+
+    
+
+}
+
+
+init();
